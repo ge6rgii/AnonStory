8717763c0a11e0856e790dfbd0c10e7662e4d9d8
ShredsGang <goshashreeeds@gmail.com>
2020-06-26 20:21:35 +0300

    static update

--- static/main.js
+++ static/main.js
@@ -19,12 +19,14 @@ fetch('http://127.0.0.1/api/vakhrushevdrift').then(function(response) {
 
 function setPrivateActive() {
   let nav = '<nav class="nav nav-masthead justify-content-center"><a class="nav-link" href="javascript:setHomeActive()">Home</a><a class="nav-link active" href="javascript:setPrivateActive()">Private Acc</a><a class="nav-link" target="_blank" href="https://teleg.run/ge6rgii">Contact</a></nav>'
-  let sessionIdInput = '';
+  let form = '<div><input class="form-control" type="text" name="cookie" placeholder="SessionID Cookie"></div><div><input class="form-control" type="text" name="username" placeholder="Username"></div><font color="#C0C0C0" size="1.5px">*Use account\'s session id, which is subscribed on this user</font><font color="#C0C0C0" size="1.5px">*Use account\'s session id, which is subscribed on this user</font>';
   document.getElementById("nav").innerHTML = nav;
-  document.getElementById("sessionid").innerHTML = sessionIdInput;
+  document.getElementById("form").innerHTML = form; 
 }
 
 function setHomeActive() {
   let nav = '<nav class="nav nav-masthead justify-content-center"><a class="nav-link active" href="javascript:setHomeActive()">Home</a><a class="nav-link"" href="javascript:setPrivateActive()">Private Acc</a><a class="nav-link" target="_blank" href="https://teleg.run/ge6rgii">Contact</a></nav>'
+  let form = '<div id="form" class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">@</span></div><input type="text" class="form-control" name="username" placeholder="username" aria-label="username" aria-describedby="basic-addon1"></div>';
   document.getElementById("nav").innerHTML = nav;
+  document.getElementById("form").innerHTML = form;
 }
