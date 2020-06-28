a30cb965ef8dc879867138560df30ff8b493c292
ShredsGang <goshashreeeds@gmail.com>
2020-06-28 18:41:47 +0300

    some code refactoring

--- static/main.js
+++ static/main.js
@@ -1,13 +1,15 @@
 function parseResponse(pics) {
   result = ""
   for (var i in pics) {
-    result = result + '<img width="450" src=' + pics[i] + '>';
+    result = result + `<div class="content"><a href="${pics[i]} target="_blank"><img height="500px" src="${pics[i]}"></a></div>`;
   }
-  document.getElementById("posts").innerHTML = result
+  document.getElementById('posts').innerHTML = result;
+  console.log(result)
 }
 
-function getStoriesLinks() {
-fetch('http://127.0.0.1/api/vakhrushevdrift').then(function(response) {
+function getStoriesLinks(username) {
+console.log(username);
+fetch(`http://127.0.0.1/api/${username}`).then(function(response) {
   response.text().then(function(text) {
     var result = text;
     var pics = JSON.parse(result)['pics'];
@@ -18,15 +20,15 @@ fetch('http://127.0.0.1/api/vakhrushevdrift').then(function(response) {
 }
 
 function setPrivateActive() {
-  let nav = '<nav class="nav nav-masthead justify-content-center"><a class="nav-link" href="javascript:setHomeActive()">Home</a><a class="nav-link active" href="javascript:setPrivateActive()">Private Acc</a><a class="nav-link" target="_blank" href="https://teleg.run/ge6rgii">Contact</a></nav>'
-  let form = '<div><input class="form-control" type="text" name="cookie" placeholder="SessionID Cookie"></div><div><input class="form-control" type="text" name="username" placeholder="Username"></div><font color="#C0C0C0" size="1.5px">*Use account\'s session id, which is subscribed on this user</font><font color="#C0C0C0" size="1.5px">*Use account\'s session id, which is subscribed on this user</font>';
-  document.getElementById("nav").innerHTML = nav;
-  document.getElementById("form").innerHTML = form; 
+  document.getElementById('home').classList.remove('active');
+  document.getElementById('private').classList.add('active');
+  document.getElementById('formHome').style.display = 'none';
+  document.getElementById('formPrivate').style.display = '';
 }
 
 function setHomeActive() {
-  let nav = '<nav class="nav nav-masthead justify-content-center"><a class="nav-link active" href="javascript:setHomeActive()">Home</a><a class="nav-link"" href="javascript:setPrivateActive()">Private Acc</a><a class="nav-link" target="_blank" href="https://teleg.run/ge6rgii">Contact</a></nav>'
-  let form = '<div id="form" class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">@</span></div><input type="text" class="form-control" name="username" placeholder="username" aria-label="username" aria-describedby="basic-addon1"></div>';
-  document.getElementById("nav").innerHTML = nav;
-  document.getElementById("form").innerHTML = form;
+  document.getElementById('home').classList.add('active');
+  document.getElementById('private').classList.remove('active');
+  document.getElementById('formHome').style.display = '';
+  document.getElementById('formPrivate').style.display = 'none';
 }
