aedeea7a6f36fadc120543606f8187a37c74efde
ShredsGang <goshashreeeds@gmail.com>
2020-06-30 19:15:58 +0300

    alerts

--- static/main.js
+++ static/main.js
@@ -1,25 +1,37 @@
-function parseResponse(pics) {
-  result = ""
+// alerts HTML
+var divClass = "alert alert-warning alert-dismissible fade show";
+
+function parseResponse(pics, videos) {
+var result = ``;
   for (var i in pics) {
-    result = result + `<div class="content"><a href="${pics[i]} target="_blank"><img height="500px" src="${pics[i]}"></a></div>`;
+    result = result + `<div class="content"><a href="${pics[i]}" target="_blank"><img height="500px" src="${pics[i]}"></a></div>`;
   }
-  document.getElementById('posts').innerHTML = result;
-  console.log(result)
+  for (var i in videos) {
+    result = result + `<div class="content"><a href="${videos[i]}" target="_blank"><video height="500px" controls="controls"><source src="${videos[i]}"></video></a></div>`;
+  }
+  return result;
 }
 
 function getStoriesLinks(username) {
-console.log(username);
 fetch(`http://127.0.0.1/api/${username}`).then(function(response) {
   response.text().then(function(text) {
-    var result = text;
-    var pics = JSON.parse(result)['pics'];
-    console.log(pics);
-    parseResponse(pics);
+    var response = text;
+    if (response == 'There aren\'t any posts or account is private') {
+      document.getElementById('posts').innerHTML = `<div class="${divClass}"><strong>${username}</strong> user doesn't exist</div>`;
+    } else if (response == 'Wrong username') {
+      document.getElementById('posts').innerHTML = `<div class="${divClass}"><strong>Ooops!</strong> It seems like there aren't any stories or this account is <a href="javascript:setPrivateActive()" color="black"><font color="#86592d" style="text-decoration: underline;">private</font></a></div>`;
+    } else {
+      var pics = JSON.parse(response)['pics'];
+      var videos = JSON.parse(response)['videos'];
+      var responseParsed = parseResponse(pics, videos);
+      document.getElementById('posts').innerHTML = responseParsed;
+    }
   });
 });
 }
 
 function setPrivateActive() {
+  document.getElementById('posts').innerHTML = '';
   document.getElementById('home').classList.remove('active');
   document.getElementById('private').classList.add('active');
   document.getElementById('formHome').style.display = 'none';
@@ -27,6 +39,7 @@ function setPrivateActive() {
 }
 
 function setHomeActive() {
+  document.getElementById('posts').innerHTML = '';
   document.getElementById('home').classList.add('active');
   document.getElementById('private').classList.remove('active');
   document.getElementById('formHome').style.display = '';
