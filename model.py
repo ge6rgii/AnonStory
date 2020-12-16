3d1281c7b8bb887fe9df866d36e9144f8c0fcbbe
ShredsGang <goshashreeeds@gmail.com>
2020-12-16 18:07:46 +0300

    model layer

---
+++ model.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class InstaClient:
+
+    def __init__(self, sessionid, user_agent):
+        self.sessionid = sessionid
+        self.user_agent = user_agent
+        url = 'some_url'
+
+    def _get_user_id(self):
+        pass
+    
+    def _parse_server_response(self):
+        pass
+
+    def get_stories_links(self):
+        pass
