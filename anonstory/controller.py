29028a66d8c4ccb8cf7b94eab35c42a5ff9b335c
ShredsGang <goshashreeeds@gmail.com>
2021-01-01 00:07:16 +0300

    ability to send the custom session id

--- anonstory/controller.py
+++ anonstory/controller.py
@@ -2,14 +2,17 @@ from flask import Flask, request
 import json
 from model import InstaClient
 from config import SESSIONID, USER_AGENT
+import logging
 
 
 app = Flask(__name__)
 instaclient = InstaClient(SESSIONID, USER_AGENT)
+logging.basicConfig(filename='log')
 
 
 @app.route('/api/public/<username>', methods=['GET'])
 def get_stories_public(username):
+    logging.info(username)
     stories_links = instaclient.get_stories_links(username)
     return stories_links
 
@@ -20,9 +23,12 @@ def get_stories_private():
     try:
         sessionid = {"sessionid": str(data["sessionid"])}
         username = str(data["username"])
-        stories_links = instaclient.get_stories_links(username, sessionid)
     except KeyError:
         return 'Plase fill out the form'
+
+    logging.info(username)
+    stories_links = instaclient.get_stories_links(username, sessionid)
+    
     return stories_links
 
 
