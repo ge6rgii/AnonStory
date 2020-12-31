fee5085fa06589b35df09be99ee5b4e8ec9319a6
ShredsGang <goshashreeeds@gmail.com>
2020-12-31 20:45:22 +0300

    config file

---
+++ anonstory/config.py
@@ -0,0 +1,3 @@
+SESSIONID = {"sessionid": ""}  # set your insta sessionid
+USER_AGENT = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu "
+                            "Chromium/79.0.3945.79 Chrome/79.0.3945.79 Safari/537.36"}
