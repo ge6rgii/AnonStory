8ae738116a4205bf19fbf51875dbfdf5e82d75cd
ShredsGang <goshashreeeds@gmail.com>
2020-12-31 21:01:03 +0300

    controller layer

--- anonstory/controller.py
+++ anonstory/controller.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 from model import InstaClient
 from config import SESSIONID, USER_AGENT
@@ -16,11 +16,13 @@ def get_stories_public(username):
 
 @app.route('/api/private', methods=['POST'])
 def get_stories_private():
+    print("**********")
+    print(request.data)
     data = json.loads(request.data)
     try:
         sessionid = {"sessionid": str(data["sessionid"])}
         username = str(data["username"])
-        stories_links = get_stories_links(username, sessionid)
+        stories_links = instaclient.get_stories_links(username, sessionid)
     except KeyError:
         return 'Plase fill out the form'
     return stories_links
